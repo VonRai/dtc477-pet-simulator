71ea30dfc1d071f790200f55a8db6fc9abbea6cc
VonRai <von.r.miller14@gmail.com>
2026-04-14 17:36:08 -0700

    WIP of Logic Steps for Stage 1

--- js/stage-food.js
+++ js/stage-food.js
@@ -1,5 +1,13 @@
-/* */ 
+/* JS for stage 1 of the game in which the creature will be thinking of different foods depicted by
+silhouettes. The user then has to choose the matching food item during 3 rounds. */ 
 
-// LOGIC STEPS
+/* LOGIC STEPS
+1. The dialogue screen is hidden and the Canvas background is changed to a kitchen.
+The creature sprite appears in the center of the Canvas.
+2. An object list stores the variables for three food items. Each food item
+has an x and y coordinate, a width and height, a base png and a silhouette png.
+3. 
+
+*/
 
 // GAME MENTOR

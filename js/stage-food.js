af0e4032dcf18dac9f8de5b8229444f129a69312
VonRai <von.r.miller14@gmail.com>
2026-04-20 22:12:46 -0700

    Updated stage-food

--- js/stage-food.js
+++ js/stage-food.js
@@ -19,32 +19,76 @@ let currentFoodIndex = 0;
 
 // Y1 is for silhouettes, Y2 is for the interactive option.
 let fooditems = [
-    { image: 'img/food1.jpg', silhouette: 'img/food1-silhouette.jpg', x: 520, y1: 250, width: 100, height: 100, name: 'Apple' },
-    { image: 'img/food2.jpg', silhouette: 'img/food2-silhouette.jpg', x: 720, y1: 250, width: 100, height: 100, name: 'Cheese' },
-    { image: 'img/food3.jpg', silhouette: 'img/food3-silhouette.jpg', x: 920, y1: 250, width: 100, height: 100, name: 'Fish' },
+    { image: 'img/food1.jpg', silhouette: 'img/food1-silhouette.jpg', x: 520, y: 550, width: 100, height: 100, name: 'Apple' },
+    { image: 'img/food2.jpg', silhouette: 'img/food2-silhouette.jpg', x: 720, y: 550, width: 100, height: 100, name: 'Cheese' },
+    { image: 'img/food3.jpg', silhouette: 'img/food3-silhouette.jpg', x: 920, y: 550, width: 100, height: 100, name: 'Fish' },
 ];
 
 let silhouetteImage = new Image();
+let correctFood = fooditems[currentFoodIndex];
+console.log(correctFood)
 silhouetteImage.src = correctFood.silhouette;
 
-let correctFood = fooditems[currentFoodIndex];
+fooditems.forEach(food => {
+    food.img = new Image();
+    food.img.src = food.image;
+});
 
 function stage1() {
     document.getElementById("dialogue").style.display = "none";
     console.log("stage1 start")
 
+    // Add event listener for `click` events.
+    canvas.addEventListener("click", function (event) {
+        let mouseX = event.offsetX;
+        let mouseY = event.offsetY;
+
+        fooditems.forEach(function (food) {
+            if (
+                mouseX > food.x &&
+                mouseX < food.x + food.width &&
+                mouseY > food.y &&
+                mouseY < food.y + food.height
+            ) {
+                if (food.name === correctFood.name) {
+
+                    currentFoodIndex++;
+
+                    if (currentFoodIndex < fooditems.length) {
+                        correctFood = fooditems[currentFoodIndex];
+                        silhouetteImage.src = correctFood.silhouette;
+                        drawStage1();
+
+                    } else {
+                        console.log("Stage complete!");
+                    }
+                }
+            }
+        });
+    });
     drawStage1()
+
 };
 
 function drawStage1() {
     ctx.fillStyle = '#9be1f2';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+    fooditems.forEach(food => {
+        ctx.drawImage(
+            food.img,
+            food.x,
+            food.y,
+            food.width,
+            food.height
+        );
+    });
+
     ctx.save();
     spriteX = canvas.width / 2 - 200;
-    spriteY = canvas.height / 2;
+    spriteY = canvas.height / 2 - 100;
     ctx.drawImage(sprite, spriteX, spriteY, 200, 200);
-    ctx.drawImage(silhouetteImage, spriteX, spriteY - 200)
+    ctx.drawImage(silhouetteImage, spriteX, spriteY - 200, 100, 100)
     ctx.restore();
 
 };
